7dafe3e3b9f36ef5047f7f01906badfbc4d32d0e
Maleick <maleick@icloud.com>
2020-12-01 14:51:26 -0600

    This is not the CME you are looking for

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ DIRPATH="/opt/Enumerate"
 EXEPATH="/opt/Enumerate/exe"
 
 ln -sf $EXEPATH/aquatone /usr/local/bin
-ln -sf $EXEPATH/cme /usr/local/bin
 ln -sf $DIRPATH/Enumerate.sh /usr/local/bin/enumerate
 
 echo "aquatone added to path"
-echo "crackmapexec added to path"
 echo "enumerate added to path"
 
